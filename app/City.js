2ffa709d4259c6efae518b6a48bafc1f605aad15
Cat Binh Le <>
2022-04-19 20:07:43 +0700

    [Develop] Added Icon

--- app/City.js
+++ app/City.js
@@ -1,7 +1,8 @@
 import react, {Component} from "react"
-import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native'
+import {View, Text, Image, TouchableOpacity, ScrollViewBase} from 'react-native'
 import styles from "./home.styles"
 import Popup from "./Popup"
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 class City extends Component {
     constructor(props) {
@@ -15,10 +16,28 @@ class City extends Component {
     render() {
 
         return (
-            <View style={{flex: 1, backgroundColor: 'red'}}>
+            <View style={{flex: 1}}>
 
+                <View style={{
+                    height: 50,
+                    margin: 16,
+                    borderRadius: 6,
+                    shadowColor: 'black',
+                    borderRadius: 8,
+                    shadowOffset: {
+                      height: 10
+                    },
+                    elevation: 10,
+                    shadowRadius: 8,
+                    shadowOpacity: 0.1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                }}>
+                    <Text>ABC</Text>
+                    <Icon name="angle-right" size={24} color="black" />
+
+                </View>
             </View>
-
         )
     }
 }
