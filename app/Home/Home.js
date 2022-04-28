67638affab45422957eac6a19322335327132c62
Bình Hồ <waungbinh@gmail.com>
2022-04-28 18:34:22 +0700

    update 18:34 28/12/2022

---
+++ app/Home/Home.js
@@ -0,0 +1,51 @@
+import react, { Component } from "react"
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native'
+import styles from "./styles";
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    state = {
+
+    }
+
+    goScreen = (item) => {
+        this.props.goScreen('DetailScreen', item)
+    }
+
+    componentDidMount() {
+        this.props.title('Home')
+    }
+
+    _renderItem = ({ item }) => (
+        <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.goScreen(item)}>
+            <Image style={styles.image} source={{ uri: item?.image }} />
+            <View style={styles.opacityInfoView} />
+            <View style={styles.infoView}>
+                <Text style={styles.txtName}>{item?.name}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+
+    render() {
+        const { param } = this.props
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    numColumns={2}
+                    showsVerticalScrollIndicator={false}
+                    data={param}
+                    renderItem={this._renderItem}
+                    keyExtractor={item => item.id}
+                />
+            </View>
+
+        )
+    }
+}
+
+export default Home
