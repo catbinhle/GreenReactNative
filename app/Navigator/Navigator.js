5d880455b1da39d8127f56ab806f97c170c97f0b
Bình Hồ <waungbinh@gmail.com>
2022-05-07 19:52:41 +0700

    update new 19:52

--- app/Navigator/Navigator.js
+++ app/Navigator/Navigator.js
@@ -6,6 +6,8 @@ import Cities from "../Cities/Cities"
 import DetailCity from "../DetailCity/DetailCity"
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import Popup from "../Popup/Popup"
+
+//install before when importing library
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
