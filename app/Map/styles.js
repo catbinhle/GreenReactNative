c7af2b143d45c13c08a16f5db7b0a2e1c64e09d1
Cat Binh Le <>
2022-05-19 15:11:52 +0700

    [develop - Baitap4] added Map

---
+++ app/Map/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Dimensions } from "react-native"
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 11  
+    },
+    map: {
+        width: '100%',
+        height: '100%'
+    }
+})
+
+export default Styles
